76d598a5166755f2108abd78868b9f9b8fb5470e
Paul Rouget <me@paulrouget.com>
2024-05-09 01:33:19 +0800

    up

--- patch.sh
+++ patch.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 cd $(dirname $0)/
 
-yj -jy < $1 | yq e ". as \$root | {} | .$2 = \$root" > patch.yaml
+# yj -ty < $1 | yq e ". as \$root | {} | .$2 = \$root" > patch.yaml
+yj -ty < $1 > patch.yaml
 yj -ty < $2.toml > to_patch.yaml
 yq '. *= load("patch.yaml")' to_patch.yaml | yj -yt > $2.toml
 rm patch.yaml to_patch.yaml
